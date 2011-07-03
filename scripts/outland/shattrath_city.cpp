33c895de8d229f3fb38d85cb9612f2e641c1339b
SeT <set@wowacadem.ru>
2011-07-03 21:53:02 +0700

    Make faster )

--- scripts/outland/shattrath_city.cpp
+++ scripts/outland/shattrath_city.cpp
@@ -426,6 +426,7 @@ struct MANGOS_DLL_DECL npc_khadgars_servantAI : public npc_escortAI
         m_uiTalkCount = 0;
         m_uiPointId = 0;
         m_uiRandomTalkCooldown = 0;
+        m_creature->SetSpeedRate(MOVE_RUN, 1.5f);
     }
 
     void MoveInLineOfSight(Unit* pWho)
